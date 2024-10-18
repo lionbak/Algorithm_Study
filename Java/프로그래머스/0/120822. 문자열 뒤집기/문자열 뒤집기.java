c084773a7b705c45e40334afa85811635711b59c
Jinwoo Bak <167261175+lionbak@users.noreply.github.com>
2024-10-18 21:14:44 +0700

    [level 0] Title: 문자열 뒤집기, Time: 0.12 ms, Memory: 77.6 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String solution(String my_string) {
+        StringBuffer answer = new StringBuffer(my_string);
+        
+        
+        return answer.reverse().toString();
+    }
+}
