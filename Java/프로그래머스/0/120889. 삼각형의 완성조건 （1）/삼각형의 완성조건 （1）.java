3bca15ffdd74cf756663a16903d933d038963ada
Jinwoo Bak <167261175+lionbak@users.noreply.github.com>
2024-12-19 12:36:20 +0900

    [level 0] Title: 삼각형의 완성조건 (1), Time: 0.48 ms, Memory: 86.1 MB -BaekjoonHub

--- Java/프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.java
+++ Java/프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.java
@@ -6,9 +6,9 @@ class Solution {
         Arrays.sort(sides);
         
         if(sides[2] < sides[1] + sides[0]){
-         answer += 1;
+         answer = 1;
         } else {
-         answer += 2;
+         answer = 2;
         }
         return answer;
     }
